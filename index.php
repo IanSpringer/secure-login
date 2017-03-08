4f5314e36d19b2b800f92556082b08d859e631f9
Robert Springer <springerdrums@gmail.com>
2017-03-08 12:36:13 -0800

    few tweaks

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require 'db.php';
 session_start();
 $_SESSION['logged_in'] = false;
-
+// $_SESSION['message'];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
@@ -68,46 +68,6 @@ require 'header.php';
 
         </div>
 
-        <div id="signup">
-          <!-- <h1>Sign Up for Free</h1> -->
-
-          <!-- <form action="index.php" method="post" autocomplete="off">
-
-          <div class="top-row">
-            <div class="field-wrap">
-              <label>
-                First Name<span class="req">*</span>
-              </label>
-              <input type="text" required autocomplete="off" name='firstname' />
-            </div>
-
-            <div class="field-wrap">
-              <label>
-                Last Name<span class="req">*</span>
-              </label>
-              <input type="text"required autocomplete="off" name='lastname' />
-            </div>
-          </div>
-
-          <div class="field-wrap">
-            <label>
-              Email Address<span class="req">*</span>
-            </label>
-            <input type="email"required autocomplete="off" name='email' />
-          </div>
-
-          <div class="field-wrap">
-            <label>
-              Set A Password<span class="req">*</span>
-            </label>
-            <input type="password"required autocomplete="off" name='password'/>
-          </div>
-
-          <button type="submit" class="button button-block" name="register" />Register</button>
-
-          </form> -->
-
-        </div>
 
       </div><!-- tab-content -->
 
